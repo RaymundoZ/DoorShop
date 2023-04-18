ecb1fbcc368ff5974dce548f96dd9670452d8d31
RaymundoZ <andrej.dron2002@gmail.com>
2023-04-18 22:07:00 +0300

    init commit

---
+++ src/main/java/com/raymundo/doorshop/DoorshopApplication.java
@@ -0,0 +1,13 @@
+package com.raymundo.doorshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoorshopApplication.class, args);
+    }
+
+}
